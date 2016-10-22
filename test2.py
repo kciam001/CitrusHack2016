af9b0c5d9334d1c87771f962e22dd19c0abb4a41
AndreCCastro <andrechristophercastro@gmail.com>
2016-10-22 16:53:13 -0700

    Can input for class search
    woohoo

--- test2.py
+++ test2.py
@@ -1,13 +1,12 @@
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-import urllib.request
-
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
+import urllib.request
 
 f = urllib.request.urlopen("https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=search")
 
@@ -20,9 +19,8 @@ driver.find_element_by_class_name("select2-arrow").click()
 text_box = driver.find_element_by_class_name("select2-input")
 text_box.send_keys('Winter 2017')
 
-
 element = WebDriverWait(driver, 10).until(
-EC.presence_of_element_located((By.XPATH, "//*[@id='201710']"))
+    EC.presence_of_element_located((By.XPATH, "//*[@id='201710']"))
                                    )
 elementList = text_box.find_elements_by_xpath("//*[@id='201710']")
 
@@ -32,10 +30,8 @@ if len(elementList) is 0:
 for i in elementList:
     i.click()
     break
-
-
-
-
+    #if att is not "":
+        #print(att)
 
 timeout = 5 # seconds
 
@@ -63,12 +59,20 @@ try:
 except TimeoutException:
     print ("Timed out waiting for page to load")
 
-text_box = driver.find_element_by_class_name("s2id_txt_subjectcoursecombo")
-text_box.send_keys('CS 100')
+text_box = driver.find_element_by_xpath("//*[@id='s2id_txt_subjectcoursecombo']/ul")
+ActionChains(driver).move_to_element_with_offset(text_box,0,0).send_keys('CS 100').perform()
+text_box.send_keys("\t")
 
 
 
-#s2id_txt_subjectcoursecombo
+
+#text_box.send_keys('CS 100')
+
+#print("Expolozo: ", uL.get_attribute('name'))
+
+
+#text_box.send_keys(Keys.ENTER)
+
 #select = driver.find_element_by_class_name("select2-results")
 #for option in select.find_elements_by_tag_name('li'):
 #    print("Text ", option.text)
