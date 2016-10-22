1da96d6880a01bc62a318fed455e959e31ec2dde
Kevin Ciampaglia <kciam001@ucr.edu>
2016-10-22 11:28:46 -0700

    Updated File

--- test2.py
+++ test2.py
@@ -5,19 +5,30 @@ import urllib.request
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-f = urllib.request.urlopen("https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=search")
+#f = urllib.request.urlopen("https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=search")
 
 driver = webdriver.Chrome()
 
-driver.get("http://www.google.ca")
+driver.get("https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=search")
 
-#text_box = driver.find_element_by_css_selector('#input')
-#text_box.send_keys('hello')
+year = input("What year do you want to search a class for?\n")
 
-text_box = driver.find_element_by_id("lst-ib")
-text_box.send_keys('hello')
+quarter = input("What quarter do you want to search a class for?\n")
 
-driver.find_element_by_name("btnG").click()
+quarter = quarter.capitalize()
+
+search_id = quarter + ' ' + year
+
+driver.find_element_by_class_name("select2-arrow").click()
+
+print(search_id)
+
+##driver.find_element_by_id("select2-result-label-2").click()
+
+##text_box = driver.find_element_by_class_name("select2-search")
+##text_box.send_keys(search_id)
+
+#driver.find_element_by_name("btnG").click()
 
 #html = str(f.read())
 
